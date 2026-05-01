6010bfe93801d7f2b8c3219e656e7a647c5e1418
nitin485 <bhardwajnitin485@gmail.com>
2026-05-02 00:05:11 +0530

    cv update

--- src/components/hero/introtext.jsx
+++ src/components/hero/introtext.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import resume from "../../assets/nitin_bhardwaj_cv.pdf";
+// import resume from "../../assets/nitin_bhardwaj_cv.pdf";
+import resume from  "../../public/nitin_bhardwaj_cv.pdf"
 
 function Text() {
   return (
